cf28ee872797ee454dc867d1749f731423a569ea
Jenny Lai <jenny.lai@flatironschool.com>
2016-12-01 01:26:06 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,16 @@
+require "pry"
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
   end
+
 
   def cobble
     self.condition = "new"
@@ -12,3 +18,5 @@ class Shoe
   end
 
 end
+
+Shoe::BRANDS
